12c147e426f77d604af6a1b1434b5aca1451d981
disjfjdizmfnkf <3037650079@qq.com>
2023-09-08 18:34:49 +0800

    Day2

---
+++ 数组/删除有序数组中的重复元素.py
@@ -0,0 +1,28 @@
+
+#     双指针
+# class Solution1:       
+#     def removeDuplicates(self, nums: List[int]) -> int:
+#         slow, fast = 0, 1
+#         while(fast < len(nums)):
+#             if nums[fast] != nums[slow]:
+#                 slow = slow + 1
+#                 nums[slow] = nums[fast]
+#             fast = fast + 1
+#         return slow + 1
+
+
+"""
+    当fast和slow所指的数不同的时候对slow+1重新赋值
+    但是如果fast和slow相邻的时候赋值是没有必要的   [1,2,3,4]
+    所以排除相邻情况 fast - slow > 1
+"""
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        slow, fast = 0, 1
+        while (fast < len(nums)):
+            if (nums[fast] != nums[slow] and fast - slow > 1): 
+                slow = slow + 1
+                nums[slow] = nums[fast]
+            fast = fast + 1
+        return slow + 1
