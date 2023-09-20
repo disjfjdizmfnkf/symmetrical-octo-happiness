bd7279810a1397df2c2691604cc0c0e2598b618a
disjfjdizmfnkf <3037650079@qq.com>
2023-09-20 10:11:09 +0800

    Day11 添加更多注释

--- 数组/存在重复元素Ⅱ.py
+++ 数组/存在重复元素Ⅱ.py
@@ -1,4 +1,6 @@
-class Solution:
+
+#HashMap 利用哈希表的特性，当添加的key重复时，覆盖对应的value
+class Solution1:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
         hash={}
         for i in range(len(nums)):
