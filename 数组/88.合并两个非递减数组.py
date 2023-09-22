761abee42d7943fd6a1e3f03059a7729bf0dc20b
disjfjdizmfnkf <3037650079@qq.com>
2023-09-22 17:04:24 +0800

    Day5 换了代码和注释

--- 数组/88.合并两个非递减数组.py
+++ 数组/88.合并两个非递减数组.py
@@ -20,17 +20,19 @@ class Solution1:
 #逆向双指针  由题意nums1后面为空，如果指针从后面遍历，把大的元素从后面开始放，就不用nums3变量
 class Solution2:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        p, q = m-1, n-1
-        i = m + n - 1
-        while q > 0:
-            if p1 >= 0 and nums1[p1] > nums2[p2]: #只有该数组不为空（注意等于0也是不为空），且比另一个大才换
-                nums1[p] = nums1[p1]  # 填入 nums1[p1]
-                p1 -= 1
+        i = m+n-1
+        while m > 0 and n > 0: #任意一个不为空（因为索引的关系）注意数组为空的条件是指针指0还是-1
+            if  nums1[m-1] < nums2[n-1]:
+                nums1[i] = nums2[n-1]
+                n-=1
             else:
-                nums1[p] = nums2[p2]  # 填入 nums2[p1]
-                p2 -= 1
-            p -= 1  # 下一个要填入的位置
-            
+                nums1[i] = nums1[m-1]
+                m-=1
+            i-=1
+        while n > 0 : #将数组2插入数组1，不确定那个先用完，这里如果数组2没完，接着换，如果不是就不用
+            nums1[i] = nums2[n-1]
+            n-=1
+            i-=1
 
 #一般双指针不能原地更改，不符合题意
 class Solution3:
