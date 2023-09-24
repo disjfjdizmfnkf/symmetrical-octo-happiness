6cce2d1e19db5b44a1bd7c1600f29f3d15d728a9
disjfjdizmfnkf <3037650079@qq.com>
2023-09-24 17:19:54 +0800

    Day7 添加注释

--- 链表/同构字符串.py
+++ 链表/同构字符串.py
@@ -5,15 +5,18 @@
 #把第s和t的每一位字母映射到同一个数字上  hashMap
 class Solution1:
     def isIsomorphic(self, s: str, t: str) -> bool:
+        """i + 1 为什么要加一? 一开始默认设定没有出现过的字符映射是0,而第一个i=0,
+        所以当之后出现的每一个新字符和i=0的字符判断都相等 [a,a]  [a,b] 会判断成相等"""
         hashMap1 = dict()
         hashMap2 = dict()
         n = len(s)
         for i in range(n):
             if hashMap1.get(s[i], 0) != hashMap2.get(t[i], 0): #如果同构，每个字符映射相同
                 return False
-            hashMap1[s[i]] = i + 1
-            hashMap2[t[i]] = i + 1  #如果都没有映射（映射相同） 映射到新数字
-        return True
+            hashMap1[s[i]] = i + 1  
+            hashMap2[t[i]] = i + 1  #如果都没有映射（映射相同） 映射到新数字   
+        return True                
+
 
 #思路同上 但是是用数组完成映射
 class Solution2:
