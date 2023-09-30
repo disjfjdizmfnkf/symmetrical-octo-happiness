2ba5d6d5a8cb074ed09b7c2b04d8b7037cc8d42a
disjfjdizmfnkf <3037650079@qq.com>
2023-10-01 00:05:44 +0800

    Day16 增加注释

--- 树/100.相同的树.py
+++ 树/100.相同的树.py
@@ -8,7 +8,7 @@
 # 递归 树的遍历 先思考怎样解决问题之后编码
 class Solution1:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if p == None or q == None:     #base case 是q和p中有一个或者两个节点为空
-            return p == q
+        if p == None or q == None:     #base case 也是边界情况
+            return p == q             #是q和p中有一个或者两个节点为空,一个为空p q不等返回false，两个为空p q相等返回true
         return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right) 
     # 不为空的时候我们需要检查三个条件
