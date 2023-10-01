c7f6202410ce7944d671849f05d04ee0401fe94c
disjfjdizmfnkf <3037650079@qq.com>
2023-10-01 19:59:50 +0800

    Day19 添加注释

--- 树/530.二叉搜索树的最小绝对差.py
+++ 树/530.二叉搜索树的最小绝对差.py
@@ -5,7 +5,7 @@
 #         self.left = left
 #         self.right = right
 
-# 中序遍历
+# 中序遍历 （从小到大）
 class Solution1:
     def inOder(self, root: TreeNode, res):
         if not root:
@@ -17,7 +17,7 @@ class Solution1:
     def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
         res = []
         minNum = float('inf')
-        self.inOder(root, res)
-        for i in range(len(res)-1):
+        self.inOder(root, res)  #中序遍历的顺序是由小到大遍历的，添加元素的顺序自然也是
+        for i in range(len(res)-1):  # 因为列表中的数是升序排列的
             minNum = min(minNum, abs(res[i] - res[i+1]))
         return minNum
