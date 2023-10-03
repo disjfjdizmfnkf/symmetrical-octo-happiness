dc5d871730e516995359c77ae476eef1084d2a50
disjfjdizmfnkf <3037650079@qq.com>
2023-10-03 20:59:01 +0800

    Day24

---
+++ 链表/141.环形链表.py
@@ -0,0 +1,24 @@
+# 双指针
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow_pointer = head
+        fast_pointer = head
+        while fast_pointer and fast_pointer.next:
+            slow_pointer = slow_pointer.next
+            fast_pointer = fast_pointer.next.next
+            if slow_pointer == fast_pointer:
+                return True
+        return False
+
+# 哈希表
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        """用节点对象当键，而不是节点对象的值，值可能会有重复但不是循环的点，而节点对象只有一个"""
+        hashMap ={}
+        curNode = head
+        while(curNode):
+            if curNode in hashMap:
+                return True
+            hashMap[curNode] = 1
+            curNode = curNode.next
+        return False
