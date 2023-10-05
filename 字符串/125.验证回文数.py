f57bdf9082fae6538cdbf5dcc716bc2114a9e771
disjfjdizmfnkf <3037650079@qq.com>
2023-10-05 17:12:07 +0800

    Day21 增加方法

--- 字符串/125.验证回文数.py
+++ 字符串/125.验证回文数.py
@@ -9,3 +9,19 @@ class Solution1:
             elif ss.isdigit():
                 temp.append(ss)
         return (res := ''.join(temp)) == res[::-1]
+
+
+# 双指针  先处理字符串
+"""
+    char.isalnum() 数字和字符返回True 其他返回False
+"""
+class Solution2:
+    def isPalindrome(self, s: str) -> bool:
+        cleanString = "".join(char.lower() for char in s if char.isalnum())
+        l, r = 0, len(cleanString) - 1
+        while l < r:
+            if cleanString[l] != cleanString[r]:
+                return False
+            l += 1
+            r -= 1
+        return True
