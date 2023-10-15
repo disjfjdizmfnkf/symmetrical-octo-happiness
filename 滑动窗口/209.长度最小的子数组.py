ebeea5d8b6f9c64bc5caae6fd624888565ea6e24
disjfjdizmfnkf <3037650079@qq.com>
2023-10-15 21:22:23 +0800

    Day36 需要更新方法，再仔细理解

---
+++ 滑动窗口/209.长度最小的子数组.py
@@ -0,0 +1,14 @@
+
+
+# 滑动窗口
+class Solution1:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        l, total = 0, 0
+        res = float("inf")
+        for r in range(len(nums)):  # 右边指针只顾往前走
+            total += nums[r]  # 向左边滑动增大值
+            while total >= target:  # 满足条件了，我们尝试去找到最小窗口
+                res = min(r - l + 1, res)
+                total -= nums[l]
+                l += 1
+        return 0 if res == float("inf") else res
