d8862d78c64e7b4753e1aff75fe90e5ef479867d
disjfjdizmfnkf <3037650079@qq.com>
2023-10-22 20:34:29 +0800

    Day42 添加方法

--- 树/257.二叉树的所有路径.py
+++ 树/257.二叉树的所有路径.py
@@ -1,17 +1,48 @@
 
-# 法一  递归 向下传递
+# dfs 深度优先搜索 左右子树向下递归  内存占用高
 class Solution1:
     def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
-        if not root:
-            return []
         res = []
-        self.dfs(root, "", res)
+        def pathse(root, path):
+            if not root:
+                return
+            path += str(root.val)
+            if not root.left and not root.right:
+                res.append(path)
+            pathse(root.left, path + '->')
+            pathse(root.right, path + '->')
+        pathse(root, '')
         return res
 
-    def dfs(self, root, ls, res):
-        if not root.left and not root.right:
-            res.append(ls + str(root.val))
-        if root.left:
-            self.dfs(root.left, ls + str(root.val) + "->", res)
-        if root.right:
-            self.dfs(root.right, ls + str(root.val) + "->", res)
+# class Solution1_:
+#     def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+#         if not root:
+#             return []
+#         res = []
+#         self.dfs(root, "", res)
+#         return res
+#
+#     def dfs(self, root, ls, res):
+#         if not root.left and not root.right:
+#             res.append(ls + str(root.val))
+#         if root.left:
+#             self.dfs(root.left, ls + str(root.val) + "->", res)
+#         if root.right:
+#             self.dfs(root.right, ls + str(root.val) + "->", res)
+
+
+
+#迭代 + stack
+class Solution2:
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        stack = [(root, '')]
+        res = []
+        while stack:
+            node, path = stack.pop()  #弹出节点，加它子节点，因为我们想要的也是叶子节点的路径
+            if not node.left and not node.right:
+                res.append(path + str(node.val))
+            if node.left:
+                stack.append((node.left, path + str(node.val) + '->'))
+            if node.right:
+                stack.append((node.right, path + str(node.val) + '->'))
+        return res
