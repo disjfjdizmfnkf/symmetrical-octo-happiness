b1b6bc3a842618b50fabd9cc937593d9e9283a8a
disjfjdizmfnkf <3037650079@qq.com>
2023-10-23 20:39:56 +0800

    Day43 理解计数排序的方法，想想思路增加灵感

--- 数组/274. H指数.py
+++ 数组/274. H指数.py
@@ -1,4 +1,4 @@
-# 计数排序
+# 排序
 class Solution1:
     def hIndex(self, citations: List[int]) -> int:
 
@@ -9,3 +9,25 @@ class Solution1:
             i += 1
             h += 1
         return h
+
+# 计数排序
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        n = len(citations)
+        count = [0] * (n + 1)  # h 引用次数 分从0到n的 n+1 种情况
+
+        for citation in citations:  # 统计 引用次数 的频次
+            # 超过n的记在n上
+            if citation >= n:
+                count[n] += 1
+            else:
+                count[citation] += 1
+
+        i = n # 要满足条件的最大h
+        h = 0
+        while i >= 0:
+            h += count[i]  # count[i] 为引用次数为i的论文个数
+            if h >= i:
+                return i
+            i -= 1
+        return 0
