8a8d82cdabdf35b974ae0883ed67761830e01000
disjfjdizmfnkf <3037650079@qq.com>
2023-10-29 22:43:46 +0800

    Day49 抽时间复习写过的代码！！！！

--- 位运算/67.二进制求和.py
+++ 位运算/67.二进制求和.py
@@ -4,7 +4,7 @@ class Solution1:
     def addBinary(self, a: str, b: str) -> str:
         x, y = int(a, 2), int(b, 2)
         while y:
-            answer = x ^ y
-            carry = (x & y) << 1
-            x, y = answer, carry
+            answer = x ^ y        # 不考虑进位的加法，有效计算最后一位
+            carry = (x & y) << 1  # 左移时自动补0
+            x, y = answer, carry  # 此时carry后面为0 不影响x ^ y
         return bin(x)[2:]
