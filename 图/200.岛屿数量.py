65ab2b549b720845754ea009cd8a9a4ebfd53a6e
disjfjdizmfnkf <3037650079@qq.com>
2023-10-30 20:08:48 +0800

    Day51 图真有趣！ 抽时间复习写过的代码！！！！

---
+++ 图/200.岛屿数量.py
@@ -0,0 +1,48 @@
+from typing import List
+"""
+给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。
+此外，你可以假设该网格的四条边均被水包围。
+示例 1：
+输入：grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]
+输出：1
+"""
+
+
+# 使用BFS找到所有的岛屿
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return
+
+        visited = set()     # 帮助分辨岛屿，输出正确的岛屿数,储存坐标
+        lands = 0
+        rows, cols = len(grid), len(grid[0])
+
+        def bfs(r, c):   # 广度优先，从点扩大到整个岛屿找到所有相连部分
+            visited.add((r, c))
+            queue = []    # bfs，dfs都要借助存储结构
+            queue.append((r, c))
+            direction = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+            while queue:
+                row, col = queue.pop(0)    # 作为队列先进先出,得到row和col是为了找相邻的点
+                for dr, dc in direction:
+                    row, col = row + dr, col + dc
+                    if (row in range(rows) and
+                        col in range(cols) and
+                        grid[row][col] == '1' and
+                        (row, col) not in visited):
+                        visited.add((row, col))
+                        queue.append((row, col)) # 别忘了入栈找相邻的相邻的
+        for r in range(rows):
+            for c in range(cols):
+                if (r, c) not in visited and grid[r][c] == '1':
+                    bfs(r, c)
+                    lands += 1
+        return lands
+
---
+++ 字典树/114.二叉树转化成链.py
@@ -0,0 +1,27 @@
+
+"""
+给你二叉树的根结点 root ，请你将它展开为一个单链表：
+
+展开后的单链表应该同样使用 TreeNode ，其中 right 子指针指向链表中下一个结点，而左子指针始终为 null 。
+展开后的单链表应该与二叉树 先序遍历 顺序相同。
+"""
+
+# 递归  因为链表展开的顺序是先序遍历的顺序，所以想最后一个节点，我们把他接到前一个节点的右子树上，
+# 然后再把前一个节点接到当前节点的右子树上，递归的这样做，最后就能得到一个展开的链表
+class Solution1:
+    def __init__(self): # 用来记录前一个节点，是全局变量
+        self.prev = None  # 初始化为None
+
+    def flatten(self, root: Optional[TreeNode]) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        if not root:
+            return
+        # 先向右再向左，因为加节点是从后往前加的
+        self.flatten(root.right)
+        self.flatten(root.left)
+
+        root.right = self.prev
+        root.left = None
+        self.prev = root
