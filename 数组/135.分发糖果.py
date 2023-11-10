42ef04f8055551d5e3d9d60292ca302ad1a61008
disjfjdizmfnkf <3037650079@qq.com>
2023-11-10 10:22:03 +0800

    Day57 修改注释

--- 数组/135.分发糖果.py
+++ 数组/135.分发糖果.py
@@ -4,10 +4,14 @@ class Solution:
         # 应该想到向前传递再向后传递来检查双方？ 为什么可以？
         n = len(ratings)
         res = [1]*n
-        for i in range(1, n):
-            if ratings[i - 1] < ratings[i]: # 从左往右， 右边大于左边
-                res[i] = res[i - 1] + 1
 
+        #  从左往右，给左边的加，保证右边比左边大的糖果多  右边大的糖果多 小的为一不变
+        for i in range(1, n):
+            if ratings[i - 1] < ratings[i]:
+                res[i] = res[i - 1] + 1  # 保证比右边大，不是简单的加1
+
+
+        # 保证左边比右边大的糖果多    左边大的糖果多(满足条件的不变，保证糖果最少)
         for i in range(n - 2, -1, -1):
             if ratings[i] > ratings[i + 1]: # 右边的是加过的判断给右边的加
                 res[i] = max(res[i], res[i + 1] + 1) # 如果res中 左边大于右边就不需要加糖果了
