6c10abedeed0f876727eb5a08b37b220f3220ff4
disjfjdizmfnkf <3037650079@qq.com>
2023-11-10 21:59:11 +0800

    Day58 明天看，今天有很重要的事情

---
+++ 位运算/190.颠倒二进制位.py
@@ -0,0 +1,7 @@
+class Solution1:
+    def reverseBits(self, n: int) -> int:
+        res = 0
+        for i in range(32):
+            res = (res << 1) | (n & 1)
+            n >>= 1
+        return res
