cb98f7373e9c8a16aa6e518b0f5f9de04d67ce4a
disjfjdizmfnkf <3037650079@qq.com>
2023-11-12 18:28:05 +0800

    Day58 改错

--- 位运算/190.颠倒二进制位.py
+++ 位运算/190.颠倒二进制位.py
@@ -1,15 +1,14 @@
-class Solution:
- """
-    &                         <<  左移一位                 >>   右移一位
-  101010   (n)                相当于乘上进制                相当于除以进制
-& 000001   (1)                  1010                        10100
----------                      10100                         1010
-  000000   (结果)
- """
 
-def reverseBits(self, n: int) -> int:
-    res = 0
-    for i in range(32):
-        res = (res << 1) | (n & 1)
-        n >>= 1
-    return res
+#     &                         <<  左移一位                 >>   右移一位
+#   101010   (n)                相当于乘上进制                相当于除以进制
+# & 000001   (1)                  1010                        10100
+# ---------                      10100                         1010
+#   000000   (结果)
+
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        res = 0
+        for i in range(32):
+            res = (res << 1) | (n & 1)
+            n >>= 1
+        return res
