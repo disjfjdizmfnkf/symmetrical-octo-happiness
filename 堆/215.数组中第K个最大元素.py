1ade22f8237ee8c5598b5a6e68e49d716c47494c
disjfjdizmfnkf <3037650079@qq.com>
2023-11-17 09:20:05 +0800

    Day63 写了一些注释

--- 堆/215.数组中第K个最大元素.py
+++ 堆/215.数组中第K个最大元素.py
@@ -6,27 +6,24 @@ picking a random element and switching it for the last element, but for arrays w
  . This eliminates processing duplicates of the pivot repeatedly. E.g.:"""
 
 
+# [2, 31, 21,3, 4, 5 , 9, 11, 21]
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        n = len(nums)
-        k = n - k
+        k = len(nums) - k   # 让第k大变成第len(nums)-k小，因为想让排序后的数组从小到大排列
 
         def quickselect(l, r):
-            pivotIndex = random.randint(l, r)
+            pivotIndex = random.randint(l, r)  # 随机找枢纽
             nums[pivotIndex], nums[r] = nums[r], nums[pivotIndex]
 
-            pivot = nums[r]
-            p = l
+            pivot, p = nums[r], l
             lessEnd = -1
             for i in range(l, r):
-                num = nums[i]
-                if num < pivot:
-                    lessEnd = p
-                if num <= pivot:
+                if nums[i] < pivot:
+                    lessEnd = p   #  比枢纽小的数字个数，也是其下标
+                if nums[i] <= pivot:       # 从左边开始，小于枢纽的和左边换，保证了小于枢纽的数字在左边
                     nums[p], nums[i] = nums[i], nums[p]
                     p += 1
-
-            nums[p], nums[r] = nums[r], nums[p]
+            nums[p], nums[r] = nums[r], nums[p]   # 把 p - 1 个比枢纽小的比较完后， 第p个位置换成枢纽
 
             if k > p:
                 return quickselect(p + 1, r)
@@ -35,4 +32,4 @@ class Solution:
             else:
                 return quickselect(l, lessEnd)
 
-        return quickselect(0, n - 1)
+        return quickselect(0, len(nums) - 1)
