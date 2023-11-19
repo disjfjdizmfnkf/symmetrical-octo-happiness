b1c447ba2c60b53aeaeb7447bcfc8194d620670c
disjfjdizmfnkf <3037650079@qq.com>
2023-11-19 19:40:51 +0800

    Day60 add new approach

--- 哈希表/49.字母异位词分组.py
+++ 哈希表/49.字母异位词分组.py
@@ -1,5 +1,22 @@
+# 两种方法没有太大的差别
 
-class Solution:
+# sorted(iterable, key=None, reverse=False)
+# list.sort(key=None, reverse=False)
+# list(iterable)   将可迭代对象转换成列表
+# separator.join(iterable) separator是连接对象的分隔符
+class Solution1:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hashMap = {}
+        for i in strs:  # 迭代的是列表对象
+            sort_i = "".join(sorted(i))
+            if sort_i in hashMap:
+                hashMap[sort_i].append(i)
+            else:
+                hashMap[sort_i] = [i]
+        return list(hashMap.values())
+
+
+class Solution2:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         res = {}
         for s in strs:
