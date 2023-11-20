1973cb18bae41608a5b72ab998d9b97487d877c2
disjfjdizmfnkf <3037650079@qq.com>
2023-11-20 22:04:42 +0800

    Day66 优化了一下

--- 双指针/11.盛最多水的容器.py
+++ 双指针/11.盛最多水的容器.py
@@ -12,3 +12,24 @@ class Solution:
             water = (r - l) * min(height[r], height[l])
             best = max(best, water)
         return best
+
+# 优化过后
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        l, r = 0, len(height) - 1
+        best = (r - l) * min(height[r], height[l])
+        while l < r:
+            if height[l] > height[r]:
+                i = r - 1
+                while l < r and height[r] > height[i]:
+                    i -= 1
+                r = i
+            else:
+                i = l + 1
+                while l < r and height[l] > height[i]:
+                    i += 1
+                l = i
+            water = (r - l) * min(height[r], height[l])
+            if best < water:
+                best = water
+        return best
