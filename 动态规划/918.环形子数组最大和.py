6e66f1ba2982a8ba10a246306a04e023d3b1ebc7
disjfjdizmfnkf <3037650079@qq.com>
2023-12-08 17:24:27 +0800

    Day81 添加了部分注释

--- 动态规划/918.环形子数组最大和.py
+++ 动态规划/918.环形子数组最大和.py
@@ -1,10 +1,9 @@
 class Solution:
     def maxSubarraySumCircular(self, nums: List[int]) -> int:
+        maxSub, minSub = nums[0], nums[0] # 初始的最值要么是计算机中的最值，**要么是变量范围内的任意值**
+        curMax, curMin = 0, 0
         total = 0
-        curMax = 0
-        curMin = 0
-        maxSub = nums[0]
-        minSub = nums[0]
+
         for i in nums:
             curMax = max(curMax + i, i)
             maxSub = max(curMax, maxSub)
@@ -12,3 +11,5 @@ class Solution:
             minSub = min(curMin, minSub)
             total += i
         return max(maxSub, total - minSub) if maxSub > 0 else maxSub # 最大值为负数
+        # maxSub 和 total - minSub 是两种情况
+        # 如果maxSub < 0 说明数组中的元素都是负数
