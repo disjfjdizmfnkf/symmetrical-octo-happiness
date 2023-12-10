cad7fe96a02904a6dcfeea625924325fc56e4bba
disjfjdizmfnkf <3037650079@qq.com>
2023-12-10 11:18:48 +0800

    Day82 学习解决DP问题的经典步骤

---
+++ 动态规划/300.最长递增子序列.py
@@ -0,0 +1,12 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        # 为问题作图分析
+        # 找到子问题
+        # 找到子问题之间的关系
+
+        # 可视化后 想到借助列表存储解决问题
+        L = [1] * len(nums)
+        for i in range(1, len(nums)):
+            sub_problem = [L[k] for k in range(i) if nums[k] < nums[i]]  # 寻找子问题
+            L[i] = 1 + max(sub_problem, default=0)  # 子问题之间的关系
+        return max(L, default=0)
