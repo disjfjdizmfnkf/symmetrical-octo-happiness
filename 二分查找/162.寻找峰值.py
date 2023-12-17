f6482e2310a1b961d4d5af673dfa9885196e4948
disjfjdizmfnkf <3037650079@qq.com>
2023-12-17 16:35:34 +0800

    Day87 修改二分查找的实现方法

--- 二分查找/162.寻找峰值.py
+++ 二分查找/162.寻找峰值.py
@@ -1,6 +1,20 @@
 
+class Solution1:
+    def findPeakElement(self, nums: List[int]) -> int:
+        l, r = 0, len(nums) - 1
+
+        # 峰值在比nums[m]大的一侧
+        while l <= r:
+            m = l + (r - l) // 2
+            if m > 0 and nums[m] < nums[m - 1]: # m = 0 时会越界
+                r = m - 1
+            elif m < len(nums) - 1 and nums[m] < nums[m + 1]: # m = len(nums) - 1 时会越界
+                l = m + 1
+            else:
+                return m
+
 # 二分查找 少判断很多边界情况
-class Solution:
+class Solution2:
     def findPeakElement(self, nums: List[int]) -> int:
         l, r = 0, len(nums) - 1
         while l + 1 < r:
--- 二分查找/34. 在排序数组中查找元素的第一个和最后一个位置.py
+++ 二分查找/34. 在排序数组中查找元素的第一个和最后一个位置.py
@@ -1,27 +1,4 @@
-# 更加模板化的解法
-class Solution1:
-    def searchRange(self, nums: List[int], target: int) -> List[int]:
-        left = self.binSerch(nums, target, True)
-        right = self.binSerch(nums, target, False)
-        return [left, right]
-    def binSerch(self, nums, target, leftBias):
-        l, r = 0, len(nums) - 1
-        i = -1
-        while l + 1 < r:
-            m = (l + r) // 2
-            if nums[m] < target:
-                l = m
-            elif nums[m] > target:
-                r = m
-            else:
-                i = m
-                if leftBias:
-                    r = m
-                else:
-                    l = m
-        return i
-
-class Solution2:
+class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         left = self.binSerch(nums, target, True)
         right = self.binSerch(nums, target, False)
