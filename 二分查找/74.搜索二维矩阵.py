470961cb408f33197960b0dbc637019e56959355
disjfjdizmfnkf <3037650079@qq.com>
2023-12-22 17:17:55 +0800

    Day90 strange

---
+++ 二分查找/74.搜索二维矩阵.py
@@ -0,0 +1,34 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        rows, cols = len(matrix), len(matrix[0])
+        # 二维的矩阵就用两个方向上的binSearch
+        top, bottom = 0, rows - 1
+        # target_row = 0
+        # while top <= bottom:
+        #     m = bottom + (top - bottom) // 2
+        #     if matrix[m][cols - 1] > target:
+        #         bottom = m - 1
+        #     elif matrix[m][cols - 1] <= target:
+        #         target_row = m
+        #         top = m + 1
+        target_row = -1
+        while top <= bottom:
+            m = top + (bottom - top) // 2
+            if matrix[m][cols - 1] < target:
+                top = m + 1
+            else:
+                target_row = m
+                bottom = m - 1
+        if target_row == -1:
+            return False
+
+        l, r = 0, cols - 1
+        while l <= r:
+            m = l + (r - l) // 2
+            if matrix[target_row][m] > target:
+                r = m - 1
+            elif matrix[target_row][m] < target:
+                l = m + 1
+            else:
+                return True
+        return False
