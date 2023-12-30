480753e9440830f602e46ba4bb684b28ad79d951
disjfjdizmfnkf <3037650079@qq.com>
2023-12-30 16:13:06 +0800

    Day95 使用python内置函数

--- 回溯/77.组合.py
+++ 回溯/77.组合.py
@@ -1,4 +1,5 @@
-class Solution:
+# 递归回溯
+class Solution1:
     def combine(self, n: int, k: int) -> List[List[int]]:
         res = []
         def backTrack(combination, n, k):
@@ -11,3 +12,9 @@ class Solution:
                 n -= 1
         backTrack([], n, k)
         return res
+
+# 运用python的内置函数
+class Solution2:
+
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        return list(itertools.combinations(range(1, n+1), k))
