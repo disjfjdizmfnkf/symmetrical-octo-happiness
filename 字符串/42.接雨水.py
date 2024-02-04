7f99c0869302dbc3d3d77fb6f72bfb27056faa94
disjfjdizmfnkf <3037650079@qq.com>
2024-02-04 23:40:42 +0800

    Day109 饭没白吃: 改bug 优化代码

--- 字符串/42.接雨水.py
+++ 字符串/42.接雨水.py
@@ -1,5 +1,6 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
+        # 想法一： 两个方向各来一遍，左边界低于或等于与有边界的水池水 加上 右边界低于于左边界的水池水
         def water_count(left, right):
             if abs(left - right) == 1:
                 return 0
@@ -21,8 +22,40 @@ class Solution:
         height = height[::-1]
         l, r = 0, 0
         while r < len(height):
-            if height[r] >= height[l]:
+            if height[r] > height[l]:
                 total += water_count(l, r)
                 l = r
-            r += 1
+            r -= 1
         return total
+
+        # 优化：遍历代替反转列表
+        # l, r = len(height) - 1, len(height) - 1
+        # while r < len(height) and l >= 0:
+        #     if height[l] > height[r]:
+        #         total += water_count(l, r)
+        #         r = l
+        #     l -= 1
+        # return total
+
+        # 优化：遍历代替反转列表和编列累计计算积水代替辅助函数
+        # total = 0
+        # left, right = 0, len(height) - 1
+        # max_left, max_right = 0, 0
+
+        # while left < right:
+        #     if height[left] < height[right]:
+        #         if height[left] > max_left:
+        #             max_left = height[left]
+        #         else:
+        #             total += max_left - height[left]
+        #         left += 1
+        #     else:
+        #         if height[right] > max_right:
+        #             max_right = height[right]
+        #         else:
+        #             total += max_right - height[right]
+        #         right -= 1
+        # return total
+
+
+        # 想法二： 从上往下一层一层削 ，未实现
