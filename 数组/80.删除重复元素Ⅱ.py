31e7fb72665ce5387945b34be61a4ec2feb8340f
disjfjdizmfnkf <3037650079@qq.com>
2024-02-28 12:30:58 +0800

    Day9/

--- 数组/80.删除重复元素Ⅱ.py
+++ 数组/80.删除重复元素Ⅱ.py
@@ -1,14 +1,14 @@
-
 # 素组是排好序的，超过两个的相同元素只保留两个
 # [0,0,1,1,1,1,2,3,3]  --> [0,0,1,1,2,3,3]
 
 
-# 类双指针
+# ???
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        i = 0   # i是存放目标元素的位置(这里指新出现的元素)
-        for num in nums:
-            if i < 2 or num > nums[i-2]: # 出现新元素的情况
-                nums[i] = num   # 存放新元素
-                i += 1          # 存放位置后移 为下一个做准备
-        return i
+        brush_index = 0  # 唯一一个对原数组操作的指针 像一个魔法刷子
+        for n in nums:
+            # 如果这个数字没有出现过两次
+            if brush_index < 2 or n != nums[brush_index - 2]:
+                nums[brush_index] = n  # 存放新元素
+                brush_index += 1  # 存放位置后移 为下一个做准备
+        return brush_index
