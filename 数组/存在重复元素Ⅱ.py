b835490e933f20f9148e7e8f80afe563899c608f
disjfjdizmfnkf <3037650079@qq.com>
2024-03-01 15:18:39 +0800

    Day12/

--- 数组/存在重复元素Ⅱ.py
+++ 数组/存在重复元素Ⅱ.py
@@ -1,14 +1,15 @@
-
-#HashMap 利用哈希表的特性，当添加的key重复时，覆盖对应的value
-class Solution1:
+# HashMap 利用哈希表的特性，当添加的key重复时，覆盖对应的value
+# 没有思路时应该画图，因为要在一定的范围内找，刚好配合哈希表的特性，同一个key可以覆盖赋值
+# 这样从左往右遍历时就可以保证hash表中的value和与它相等的数字是最近的
+class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        hash={}
-        for i in range(len(nums)):
-            if(nums[i] not in hash):
-                hash[nums[i]]=i
+        hashMap = {}
+        for index, num in enumerate(nums):
+            if num not in hashMap:
+                hashMap[num] = index
             else:
-                if(i-hash[nums[i]]<=k):
+                if abs(hashMap[num] - index) <= k:
                     return True
                 else:
-                    hash[nums[i]]=i
+                    hashMap[num] = index
         return False
