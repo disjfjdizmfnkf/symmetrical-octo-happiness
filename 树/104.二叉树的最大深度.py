42898298a53a1a9967a1646c6103151ace0072af
disjfjdizmfnkf <3037650079@qq.com>
2024-03-12 21:55:11 +0800

    Day18/ 应该没必要写了

--- 树/104.二叉树的最大深度.py
+++ 树/104.二叉树的最大深度.py
@@ -11,4 +11,4 @@ class Solution1:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
-        return max(1 + self.maxDepth(root.left), 1 + self.maxDepth(root.right)) 
+        return max(self.maxDepth(root.left) + 1, self.maxDepth(root.right) + 1)
