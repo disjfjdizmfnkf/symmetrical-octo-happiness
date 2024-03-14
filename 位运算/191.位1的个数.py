4f0b7c96d812d3af2cf57066a864a09e70b5248f
disjfjdizmfnkf <3037650079@qq.com>
2024-03-14 22:32:32 +0800

    Day22

--- 位运算/191.位1的个数.py
+++ 位运算/191.位1的个数.py
@@ -1,4 +1,10 @@
 
+# 对于一个多位二进制数进行取模%2操作，实际上是在检查
+# 这个数的最低位 (最右边的位) 是否为1，这是由二进制
+# 数的性质决定的，和十进制取模%2是一样的这也是判断二
+# 进制数奇偶性的一种方法
+
+
 
 # & 逻辑与    Link：https://www.youtube.com/watch?v=5Km3utixwZs
 class Solution1:
@@ -16,7 +22,7 @@ class Solution2:
     def hammingWeight(self, n: int) -> int:
         res = 0
         while n:
-            res += n%2 #模运算结果为最后一个数
+            res += n%2
             n >> 1
 
         return res
