e3b51da25a6c29008665ad3ad148f839e89188b8
disjfjdizmfnkf <3037650079@qq.com>
2024-03-23 22:58:29 +0800

    Day26/

--- 数组/55.跳跃游戏.py
+++ 数组/55.跳跃游戏.py
@@ -11,16 +11,15 @@ class Solution2:
             return True
         return False
 
-
-# 改变遍历范围
+# 改变遍历范围(不行)
 class Solution1:
     def canJump(self, nums: List[int]) -> bool:
-        dis = 0   # 记录此时可以达到的最远距离
-        for i in range(len(nums)):  # 为什么不能直接把范围改成 range（dis）
+        i = dis = 0   # 记录此时可以达到的最远距离
+        while i < len(nums): # for i in range(len(nums)): 会立即生成一个不可变的序列
             if i > dis:  #  i一定在此时能到的最大范围内
                 break
             dis = max(dis, i + nums[i])
             if dis >= len(nums) - 1:
                 return True
+            i += 1
         return False
-
