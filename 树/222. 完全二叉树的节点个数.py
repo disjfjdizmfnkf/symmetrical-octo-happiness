947876b1b24235a4211fe6f71d2e0c6e4a67a93c
disjfjdizmfnkf <3037650079@qq.com>
2024-03-26 21:09:32 +0800

    Day30/

--- 树/222. 完全二叉树的节点个数.py
+++ 树/222. 完全二叉树的节点个数.py
@@ -1,10 +1,6 @@
-
-
 # 递归
 class Solution1:
     def countNodes(self, root: Optional[TreeNode]) -> int:
-        def countHelper(root, n):
-            if not root:
-                return 0
-            return countHelper(root.left, n) + countHelper(root.right, n) + 1
-        return countHelper(root, 0)
+        if not root:
+            return 0
+        return self.countNodes(root.right) + self.countNodes(root.left) + 1
