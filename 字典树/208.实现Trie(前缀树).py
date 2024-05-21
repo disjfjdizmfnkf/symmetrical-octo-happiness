1acbe678cbd48fd8fad1a9affb5c93d5ec88073c
disjfjdizmfnkf <3037650079@qq.com>
2024-05-21 23:14:00 +0800

    Day47/ 改错

--- 字典树/208.实现Trie(前缀树).py
+++ 字典树/208.实现Trie(前缀树).py
@@ -1,19 +1,19 @@
-class TrieNode:  # 一开始的根并没有值，但是之后的每个node都会有值，因为他们都是children
+class TrieNode:
     def __init__(self):
-        self.children = {} # 毫无疑问使用hash表而不是26个长度的列表
+        self.children = {}
         self.isEnd = False
 
 
 class Trie:
 
     def __init__(self):
-        self.root = TrieNode
+        self.root = TrieNode()  # 加上括号调用构造函数，创建的是实例对象，否者初始化为类本身了
 
     def insert(self, word: str) -> None:
         cur = self.root
         for char in word:
             if char not in cur.children:
-                cur.children[char] = TrieNode()  #  每个都是TrieNode
+                cur.children[char] = TrieNode()
             cur = cur.children[char]
         cur.isEnd = True
 
@@ -21,16 +21,23 @@ class Trie:
         cur = self.root
         for char in word:
             if char in cur.children:
-                cur = cur.children
+                cur = cur.children[char]
             else:
                 return False
         return cur.isEnd
 
     def startsWith(self, prefix: str) -> bool:
-        cur =  self.root
+        cur = self.root
         for char in prefix:
             if char in cur.children:
-                cur = cur.children
+                cur = cur.children[char]
             else:
                 return False
         return True
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
