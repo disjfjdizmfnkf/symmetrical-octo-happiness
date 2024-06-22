ecf7445362e13a1b6308cd5b2b3fb348f6410dfa
disjfjdizmfnkf <3037650079@qq.com>
2024-06-22 16:57:13 +0800

    Day63/

--- 动态规划/198.打家劫舍.py
+++ 动态规划/198.打家劫舍.py
@@ -1,23 +1,50 @@
+from typing import List
+
+
 ########################################################################################################
-#                                        得到最终答案的过程
-#  1. 思考问题了解递归的过程     2.使用一般的递归解法    3.使用记忆优化递归
-#  4. 将递归改为迭代           5.使用变量替代记忆(动态规划)
+#  1.确定递推状态(递推公式中f(n)的含义)
+#  2.确定递推公式:f(x)和哪些f(y)
+#  3.分析边界条件
+#  4.动态规划就是多了一个决策过程
 ########################################################################################################
 
-# 用递归理解动态规划
+
+class Solution:
+    # f(n)代表到前n家获取的最大值；f(n) = max(f(n-1),  f(n-2) + gain)
+    def rob0(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [0] * (n + 1)
+        dp[0], dp[1] = 0, nums[0]
+        for i in range(2, n + 1):  # range不包含最后一个n+1
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i - 1])  # nums[i-1]：i-1是第n家的索引
+        return dp[n]
+
+    # rob0优化
+    def rob1(self, nums: List[int]) -> int:
+        rub1, rub2 = 0, 0
+        # [rub1, rub2, n, n+1, ...]
+        for n in nums:
+            temp = max(rub1 + n, rub2)  # temp 为可偷到的最大金额 rub1 + n 为偷当前房子，rub2 为不偷当前房子 只有这两种情况
+            rub1 = rub2
+            rub2 = temp
+        return rub2
+
+
+# 递归
 # 返回p之后可以强到的最大金额
 class Solution1:
-    def rob(self, nums: List[int]) -> int:
+    def rob0(self, nums: List[int]) -> int:
         def helper(nums, p):
             if p >= len(nums):
                 return 0
             return max(nums[p] + helper(nums, p + 2), helper(p + 1))  # 画出决策树 只有抢和不抢当前的两种情况
+
         return helper(nums, 0)
 
-# 递归 + 记忆 优化
-class Solution2:
-    def rob(self, nums: List[int]) -> int:
+    # 递归 + 记忆 优化
+    def rob1(self, nums: List[int]) -> int:
         memo = {}
+
         def helper(nums, p):
             if p >= len(nums):
                 return 0
@@ -25,26 +52,5 @@ class Solution2:
                 return memo[p]
             memo[p] = max(nums[p] + helper(nums, p + 2), helper(p + 1))
             return memo[p]
+
         return helper(nums, 0)
-
-# 迭代 + 记忆
-# 使用数组来记录抢不抢当前位置的两种情况
-class Solution3:
-    def rob(self, nums: List[int]) -> int:
-        memo = {}
-        memo[0], memo[1] = 0, nums[0]
-        for i in range(len(nums)):
-            memo[i + 1] = max(memo[i], memo[i - 1] + nums[i]) # memo[i]是不抢当前  memo[i - 1] + nums[i] 是抢当前
-        return memo[len(nums)]
-
-# 使用变量代替memo  动态规划
-class Solution:
-    def rob(self, nums: List[int]) -> int:
-        rub1, rub2 = 0, 0
-
-        # [rub1, rub2, n, n+1, ...]
-        for n in nums:
-            temp = max(rub1 + n, rub2)  # temp 为可偷到的最大金额 rub1 + n 为偷当前房子，rub2 为不偷当前房子 只有这两种情况
-            rub1 = rub2
-            rub2 = temp
-        return rub2
