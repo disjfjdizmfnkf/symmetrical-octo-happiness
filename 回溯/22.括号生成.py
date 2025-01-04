d2d68dd364071958384255a9720fff694a300e48
disjfjdizmfnkf <3037650079@qq.com>
2025-01-04 10:00:12 +0800

    fix: 超出内存限制

--- 回溯/22.括号生成.py
+++ 回溯/22.括号生成.py
@@ -1,37 +1,41 @@
-class Solution1:
+class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         res = []
 
-        def backTrack(combine, l_brackets, r_brackets, n):
-            if r_brackets > l_brackets:
-                return
-            if r_brackets == n:
+        def backTrack(combine, l_brackets, r_brackets):
+            # base case
+            if l_brackets == n and r_brackets == n:
                 res.append(combine)
-            # 在递归调用中 combine + "(" 实际上是new_combine = combine + "("
-            # 创建了一个新的字符串，而不是在原有的字符串上修改，所以会消耗更多的内存
-            backTrack(combine + "(", l_brackets + 1, r_brackets, n)
-            backTrack(combine + ")", l_brackets, r_brackets + 1, n)
+                return
+        
+            # 左括号数量小于 n 时都可以添加左括号
+            if l_brackets < n:
+                backTrack(combine + "(", l_brackets + 1, r_brackets)
+        
+            # 只能添加右括号，如果右括号数量小于左括号数量
+            if r_brackets < l_brackets:
+                backTrack(combine + ")", l_brackets, r_brackets + 1)
 
-        backTrack("", 0, 0, n)
+        backTrack("", 0, 0)
         return res
-
 
 class Solution2:
     def generateParenthesis(self, n: int) -> List[str]:
         res = []
+        combine = []
         # 使用列表来模拟栈，pop()和append()操作
         # 递归的时候用列表作为combine，这样在递归调用的时候不会再创建新的字符串
-        def backTrack(combine, l_brackets, r_brackets):
+        def backTrack(l_brackets, r_brackets):
             if r_brackets == n:
                 res.append("".join(combine))
             if r_brackets < l_brackets:
                 combine.append(')')
-                backTrack(combine, l_brackets, r_brackets + 1)
+                backTrack(l_brackets, r_brackets + 1)
                 combine.pop()
             if l_brackets < n:
                 combine.append('(')
-                backTrack(combine, l_brackets + 1, r_brackets)
+                backTrack(l_brackets + 1, r_brackets)
                 combine.pop()
 
-        backTrack([], 0, 0)
+        backTrack(0, 0)
         return res
