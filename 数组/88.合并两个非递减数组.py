8f3f6ed318a4361daa58b3bd571113afe59973a5
disjfjdizmfnkf <3037650079@qq.com>
2025-01-12 22:51:52 +0800

    复习: Day6

--- 数组/88.合并两个非递减数组.py
+++ 数组/88.合并两个非递减数组.py
@@ -1,21 +1,17 @@
-# 给你两个按 非递减顺序 排列的整数数组 nums1 和 nums2，
-# 另有两个整数 m 和 n ，分别表示 nums1 和 nums2 中的元素数目。
-# 请你 合并 nums2 到 nums1 中，使合并后的数组同样按 非递减顺序 排列。
-# 注意：最终，合并后数组不应由函数返回，而是存储在数组 nums1 中。
-# 为了应对这种情况，nums1 的初始长度为 m + n，其中前 m 个元素表示应合并的元素，后 n 个元素为 0 ，应忽略。nums2 的长度为 n 。
-# 示例 1：
-# 输入：nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
-# 输出：[1,2,2,3,5,6]
-# 解释：需要合并 [1,2,3] 和 [2,5,6] 。
-# 合并结果是 [1,2,2,3,5,6] ，其中斜体加粗标注的为 nums1 中的元素。
-
-
-# 先合并再排序
 class Solution1:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        nums1[m:] = nums2
-        nums1.sort()
-
+        p = m + n - 1
+        p1 = m - 1
+        p2 = n - 1
+        while p2 >= 0:  # nums2没有合并完
+            # 如果 p1 < 0，那么走 else 分支，把 nums2 合并到 nums1 中
+            if nums1[p1] > nums2[p2] and p1 >= 0:
+                nums1[p] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[p] = nums2[p2]
+                p2 -= 1
+            p -= 1
 
 #逆向双指针  由题意nums1后面为空，如果指针从后面遍历，把大的元素从后面开始放，就不用nums3变量
 class Solution2:
@@ -34,24 +30,4 @@ class Solution2:
             n-=1
             i-=1
 
-#一般双指针不能原地更改，不符合题意
-class Solution3:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        sorted = []
-        p1, p2 = 0, 0
-        while p1 < m or p2 < n:
-            if p1 == m:
-                sorted.append(nums2[p2])
-                p2 += 1
-            elif p2 == n:
-                sorted.append(nums1[p1])
-                p1 += 1
-            elif nums1[p1] < nums2[p2]:
-                sorted.append(nums1[p1])
-                p1 += 1
-            else:
-                sorted.append(nums2[p2])
-                p2 += 1
-        nums1[:] = sorted
-            
         
--- 滑动窗口/3.无重复字符的最长字串.py
+++ 滑动窗口/3.无重复字符的最长字串.py
@@ -4,8 +4,8 @@ class Solution1:
         hashMap = {}
         ans = l = r = 0
         for r in range(len(s)):
-            if s[r] in hashMap:
-                l = max(l, hashMap[s[r]] + 1)  # 防止左边界回退
+            if s[r] in hashMap:  # 在哈希表中不一定和现在窗口中的重复，s[r]在哈希表中但是在l之前，说明其实不是重复字符实际不在窗口中
+                l = max(l, hashMap[s[r]] + 1)
             ans = max(ans, r - l + 1)
             hashMap[s[r]] = r
         return ans
@@ -13,7 +13,7 @@ class Solution1:
 class Solution2:
     def lengthOfLongestSubstring(self, s: str) -> int:
         memo = set()
-        res = 0
+        res, l = 0
         for r in range(len(s)):
             # abcdeff 如果添加到第二个f,就要依次从头删除到第一个f
             while s[r] in memo:
