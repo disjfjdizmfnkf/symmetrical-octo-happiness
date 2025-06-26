1bf7603b79ba097083de03aaa0af8e585244bd7d
zhangheyu <zhangheyu_i@didiglobal.com>
2025-06-26 16:09:30 +0800

    fix: isNumber in 394

--- 图/994.腐烂的橘子.js
+++ 图/994.腐烂的橘子.js
@@ -1,38 +1,41 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
 var orangesRotting = function (grid) {
-  const R = grid.length, C = grid[0].length;
-  const dr = [-1, 0, 1, 0];
-  const dc = [0, -1, 0, 1];
-  const queue = [];
-  const depth = new Map();
+  let rows = grid.length, cols = grid[0].length;
+  let directions = [[-1, 0], [0, -1], [1, 0], [0, 1]];
+  let queue = [];  // 存放腐烂橘子坐标
+  let depth = new Map();  // 存放每个橘子腐烂的时间
 
-  for (let r = 0; r < R; ++r) {
-    for (let c = 0; c < C; ++c) {
-      if (grid[r][c] === 2) {
-        const code = r * C + c;
-        queue.push(code);
-        depth.set(code, 0);
+  for (const i = 0; i < rows; i++) {
+    for (const j = 0; j < cols; j++) {
+      // 将烂橘子加入队列
+      if (grid[i][j] === 2) {
+        let code = i * cols + j;
+        queue.push(code);  // NOTE: 使用线性存储
+        depth.set(code, 0);  // 初始化深度为0
       }
     }
-  }
+  };
 
-  let ans = 0;
-  while (queue.length !== 0) {
-    const code = queue.shift();
-    const r = Math.floor(code / C), c = code % C;
-    for (let k = 0; k < 4; ++k) {
-      const nr = r + dr[k];
-      const nc = c + dc[k];
-      if (0 <= nr && nr < R && 0 <= nc && nc < C && grid[nr][nc] === 1) {
+  const ans = 0;
+  while (queue.length > 0) {
+    let code = queue.shift();
+    let r = Math.floor(code / cols), c = code % cols;
+    for (let d of directions) {
+      let nr = r + d[0], nc = c + d[1];
+      if (nr >= 0 && nr < rows && nc >= 0 && nc < cols && grid[nr][nc] === 1) {
         grid[nr][nc] = 2;
-        const ncode = nr * C + nc;
-        queue.push(ncode);
-        depth.set(ncode, depth.get(code) + 1);
-        ans = depth.get(ncode);
+        let newCode = nr * cols + nc;
+        queue.push(newCode);
+        depth.set(newCode, depth.get(code) + 1);
+        ans = depth.get(newCode);
       }
+
     }
   }
 
-  const freshOrangesCount = grid.reduce((acc, row) => acc + row.reduce((acc, v) => acc + (v === 1 ? 1 : 0), 0), 0);
-  return freshOrangesCount > 0 ? -1 : ans;
-};
-
+  const refreshCount = grid.reduce((acc, row) => acc + row.reduce((acc, v) => acc + (v === 1 ? 1 : 0), 0), 0);
+  return refreshCount > 0 ? -1 : ans;
+}
